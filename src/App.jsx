250075a7fb20ee6c63386729d922313ba6fc8e17
Nikhil Yadav <facts.foundr@gmail.com>
2023-09-19 19:53:09 +0530

    add folders

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Icons from './Icons';
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Icons/>
+      <Footer/>
+
+    </>
+  );
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <div className="bg-[#1b1a1a] h-[10vh]">
+<h1 className="text-white font-md px-5 mb-0 py-10"></h1>
+        </div>
+    )
+}
