6815a502add4595cbbd203786b9fd778d9f0b73c
Nikhil Yadav <facts.foundr@gmail.com>
2023-10-09 08:15:48 +0530

    add Login btn

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -3,31 +3,33 @@ export default function Navbar() {
     <nav className="bg-[#1b1a1a]  duration-300 ease-out fixed text-capitalize text-white flex py-4 px-4 w-full justify-between items-center  h-[5vh]">
       <div className="font-bold text-xl">Portfolio.</div>
 
-      <ul className="text-sm px-4 py-1 ">
-        <a
+      <ul className="text-sm px-4 py-1 flex justify-between items-center ">
+        <li
           className="pr-10 hover:underline decoration-pink-500 decoration-1 underline-offset-4"
-          href="#"
+         
         >
           HOME
-        </a>
-        <a
+        </li>
+        <li
           className="pr-10 hover:underline decoration-pink-500 decoration-1 underline-offset-4"
-          href="#"
+          
         >
           PROJECTS
-        </a>
-        <a
-          className="pr-10 hover:underline decoration-pink-500 decoration-1 underline-offset-4"
-          href="#"
+        </li>
+        <li
+          className="pr-10 hover:underline decoration-pink-500 decoration-1 underline-offset-4"  
         >
           SKILLS
-        </a>
-        <a
+        </li>
+        <li
           className="pr-10 hover:underline  decoration-pink-500 decoration-1 underline-offset-4"
-          href="#"
+          
         >
           CONTACT
-        </a>
+        </li>
+        <li>
+          <button className="text-md drop-shadow-lg decoration-purple-300  bg-purple-500 px-6 py-2">Login</button>
+        </li>
       </ul>
     </nav>
   );
